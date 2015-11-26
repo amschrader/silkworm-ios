e1d460a279d3fd1880422082040d424202fd02f6
Allyson Schrader <amschrader@gmail.com>
2015-11-25 17:33:47 -0800

    Adds bridging header for obj-c pods

---
+++ Silkworm/Silkworm-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Silkworm-Bridging-Header.h
+//  Silkworm
+//
+//  Created by Allyson Schrader on 11/25/15.
+//  Copyright © 2015 Allyson Schrader. All rights reserved.
+//
+
+#import <AFOAuth1Client/AFOAuth1Client.h>
